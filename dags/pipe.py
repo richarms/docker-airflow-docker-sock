491e8c60491fc02e67f3bf7a3ae203c29e6e9668
richarms <richarms@ska.ac.za>
2019-09-02 14:50:19 +0200

    initial commit of project

---
+++ dags/pipe.py
@@ -0,0 +1,104 @@
+import logging
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+import docker
+from docker import Client
+
+log = logging.getLogger(__name__)
+
+default_args = {
+    'owner': 'richarms',
+    'start_date': datetime(2019, 8, 29),
+}
+
+def read_xcoms(**context):
+    for idx, task_id in enumerate(context['data_to_read']):
+        data = context['task_instance'].xcom_pull(task_ids=task_id, key='data')
+        logging.info(f'[{idx}] I have received data: {data} from task {task_id}')
+
+def launch_docker_container(**context):
+    # just a mock for now
+    #logging.info(context['ti'])
+    #logging.info(context['image_name'])
+    #my_id = context['my_id']
+    #context['task_instance'].xcom_push('data', f'my name is {my_id}', context['execution_date'])
+
+    image_name = context['image_name']
+    client: Client = docker.from_env()
+
+    log.info(f"Creating image {image_name}")
+    container = client.create_container(image=image_name)
+
+    container_id = container.get('Id')
+    log.info(f"Running container with id {container_id}")
+    client.start(container=container_id)
+
+    logs = client.logs(container_id, follow=True, stderr=True, stdout=True, stream=True, tail='all')
+
+    try:
+        while True:
+            l = next(logs)
+            log.info(f"Task log: {l}")
+    except StopIteration:
+        pass
+        
+    inspect = client.inspect_container(container)
+    log.info(inspect)
+    if inspect['State']['ExitCode'] != 0:
+                raise Exception("Container has not finished with exit code 0")
+
+    log.info(f"Task ends!")
+    my_id = context['my_id']
+    context['task_instance'].xcom_push('data', f'my name is {my_id}', context['execution_date'])
+
+
+def do_test_docker():
+    client = docker.from_env()
+    for container in client.containers():
+        logging.info(str(container))
+
+with DAG('pipeline_docker', default_args=default_args) as dag:
+    t1 = BashOperator(
+        task_id='print_date1',
+        bash_command='date')
+
+    t2_1_id = 'do_task_one'
+    t2_1 = PythonOperator(
+        task_id=t2_1_id,
+        provide_context=True,
+        op_kwargs={
+            'image_name': 'task1',
+            'my_id': t2_1_id
+        },
+        python_callable=launch_docker_container
+    )
+
+    t2_2_id = 'do_task_two'
+    t2_2 = PythonOperator(
+        task_id=t2_2_id,
+        provide_context=True,
+        op_kwargs={
+            'image_name': 'task2',
+            'my_id': t2_2_id
+        },
+        python_callable=launch_docker_container
+    )
+
+    t3 = PythonOperator(
+        task_id='read_xcoms',
+        provide_context=True,
+        python_callable=read_xcoms,
+        op_kwargs={
+            'data_to_read': [t2_1_id, t2_2_id]
+        }
+    )
+
+    t1_5 = PythonOperator(
+        task_id="test_docker",
+        python_callable=do_test_docker
+    )
+
+    t1 >> t1_5 >> [t2_1, t2_2] >> t3
